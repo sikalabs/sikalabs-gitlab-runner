188a16dbf5960873725a1e8da344f43b875a4bcc
Ondrej Sika <ondrej@ondrejsika.com>
2020-11-19 14:34:19 +0100

    create-runner - map /etc/gitlab-runner to host

--- create-runner.sh
+++ create-runner.sh
@@ -3,7 +3,7 @@
 docker run -d \
     --name gitlab-runner \
     --restart always \
+    -v /etc/gitlab-runner:/etc/gitlab-runner \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /builds:/builds \
     gitlab/gitlab-runner:latest
-
